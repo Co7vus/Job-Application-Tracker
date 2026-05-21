8c5bc10fe09fe6be4e2e13ca0217700973b704d5
Co7vus <disanduvitharana7@gmail.com>
2026-05-21 21:35:03 +0800

    Updated CORS for Production

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",  "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173",  "http://127.0.0.1:5173", "https://job-application-tracker-nine-indol.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
